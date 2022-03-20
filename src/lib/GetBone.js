ea2225ef389c562f578c66388aadd35c7d85be88
Trevor <turbits@protonmail.com>
2022-03-20 14:11:07 -0400

    genericize(?) bone element creation

--- src/lib/GetBone.js
+++ src/lib/GetBone.js
@@ -1,95 +1,88 @@
-import { Image } from "@chakra-ui/react";
-import coinHeads from '../assets/bone_coin_heads.png';
-import coinTails from '../assets/bone_coin_tails.png';
-import d4 from '../assets/bone_d4.png';
-import d6 from '../assets/bone_d6.png';
-import d8 from '../assets/bone_d8.png';
-import d10 from '../assets/bone_d10.png';
-import d12 from '../assets/bone_d12.png';
-import d20 from '../assets/bone_d20.png';
-import dx from '../assets/bone_dx.png';
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
 
-export const HandleGetBoneImage = (value) => {
-  switch (value) {
+import coinHeads from "../assets/bone_coin_heads.png";
+import coinTails from "../assets/bone_coin_tails.png";
+import d10 from "../assets/bone_d10.png";
+import d12 from "../assets/bone_d12.png";
+import d20 from "../assets/bone_d20.png";
+import d4 from "../assets/bone_d4.png";
+import d6 from "../assets/bone_d6.png";
+import d8 from "../assets/bone_d8.png";
+import dx from "../assets/bone_dx.png";
+
+export const GetBoneElement = (faceCount, value) => {
+  switch (faceCount) {
     case 2:
-      return GetCoinBoneImage();
+      return GetCoinBoneElement(value);
     case 4:
-      return GetD4BoneImage();
+      return GetD4BoneElement(value);
     case 6:
-      return GetD6BoneImage();
+      return GetD6BoneElement(value);
     case 8:
-      return GetD8BoneImage();
+      return GetD8BoneElement(value);
     case 10:
-      return GetD10BoneImage();
+      return GetD10BoneElement(value);
     case 12:
-      return GetD12BoneImage();
+      return GetD12BoneElement(value);
     case 20:
-      return GetD20BoneImage();
+      return GetD20BoneElement(value);
     default:
-      return GetDXBoneImage(value);
+      return GetDXBoneElement(value);
   }
 };
 
-export const GetCoinBoneImage = () => {
-  return <Image src={} w="100%" />;
+export const GetCoinBoneElement = (value) => {
+  switch (value) {
+    case 1:
+      return BoneElement(0, coinHeads);
+    case 2:
+      return BoneElement(0, coinTails);
+    default:
+      break;
+  }
 };
 
-export const GetD4BoneImage = () => {
-  return <Image src={} w="100%" />;
+export const GetD4BoneElement = (value) => {
+  return BoneElement(value, d4);
 };
 
-export const GetD6BoneImage = () => {
-  return <Image src={} w="100%" />;
+export const GetD6BoneElement = (value) => {
+  return BoneElement(value, d6);
 };
 
-export const GetD8BoneImage = () => {
-  return <Image src={} w="100%" />;
+export const GetD8BoneElement = (value) => {
+  return BoneElement(value, d8);
 };
 
-export const GetD10BoneImage = () => {
-  return <Image src={} w="100%" />;
+export const GetD10BoneElement = (value) => {
+  return BoneElement(value, d10);
 };
 
-export const GetD12BoneImage = () => {
-  return <Image src={} w="100%" />;
+export const GetD12BoneElement = (value) => {
+  return BoneElement(value, d12);
 };
 
-export const GetD20BoneImage = () => {
-  return <Image src={} w="100%" />;
+export const GetD20BoneElement = (value) => {
+  return BoneElement(value, d20);
 };
 
-export const GetDXBoneImage = (value) => {
-  return <Image src={} w="100%" />;
+export const GetDXBoneElement = (value) => {
+  return BoneElement(value, dx);
 };
 
-// export const BoneElement = (value) => {
-//   return (
-//     <Button
-//       width="96px"
-//       height="96px"
-//       key={index}
-//       bg={colorMode === "light" ? "gray.200" : "gray.700"}
-//       padding={1}
-//       position="relative"
-//       value={value}
-//       cursor="pointer"
-//       onClick={() => handleRemoveBone(index)}
-//       m="5px"
-//     >
-//       <Flex w="100%" h="100%" align="center" justify="center">
-//         {value === 20 && <Image src={boneDi} w="100%" />}
-//         {value % 3 === 0 && value !== 20 && <Image src={boneTri} w="100%" />}
-//         {value % 3 !== 0 && value !== 20 && <Image src={bone} w="100%" />}
-//         <Text
-//           pos="absolute"
-//           left="50%"
-//           top="50%"
-//           transform="translate(-50%, -50%)"
-//           color="gray.800"
-//         >
-//           {value}
-//         </Text>
-//       </Flex>
-//     </Button>
-//   );
-// };
+export const BoneElement = (value, graphic) => {
+  return (
+    <Box w="100%" h="100%">
+      <Image src={graphic} w="100%" />
+      <Text
+        pos="absolute"
+        left="50%"
+        top="50%"
+        transform="translate(-50%, -50%)"
+        color="gray.800"
+      >
+        {value === 0 ? "" : value}
+      </Text>
+    </Box>
+  );
+};
