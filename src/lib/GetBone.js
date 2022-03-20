308cb16cac6df182e422db31a29185a6a8cb7990
Trevor <turbits@protonmail.com>
2022-03-20 17:48:46 -0400

    Update GetBone.js

--- src/lib/GetBone.js
+++ src/lib/GetBone.js
@@ -1,4 +1,4 @@
-import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import { Box, Image, Text } from "@chakra-ui/react";
 
 import coinHeads from "../assets/bone_coin_heads.png";
 import coinTails from "../assets/bone_coin_tails.png";
